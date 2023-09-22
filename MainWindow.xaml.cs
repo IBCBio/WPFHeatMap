cc4a4aef1d1ca8fc5ea1bdd8f160cfebaa7b0c6f
Iván Jiménez <908544+ivanjimenez@users.noreply.github.com>
2023-09-22 19:06:50 +0200

    creando el heatmap

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using System.Timers;
 using System.Windows;
 
 namespace WPFHeatMap
@@ -6,52 +8,85 @@ namespace WPFHeatMap
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+	///
+
 	public partial class MainWindow : Window
 	{
+		
+		Random rRand = new Random();
+
+		// Loop variables
+		int x;
+		int y;
+		byte intense;
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			cbColors.SelectedIndex = 0;
+			cbColors.SelectedIndex = 4;
+
+			
 		}
 
-		private void Window_Loaded(object sender, RoutedEventArgs e)
+	
+
+	private async void Window_Loaded(object sender, RoutedEventArgs e)
 		{
-			RenderContent();
+			await Dispatcher.Invoke(async () => { RenderContent(); });
+			
 		}
 
-		private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+		private async void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
 		{
-			RenderContent();
+			await Dispatcher.Invoke(async () => { RenderContent(); });
 		}
 
 		/// <summary>
 		/// Renders random heat map
 		/// </summary>
-		private void RenderContent()
+		private async void RenderContent()
 		{
-			cHeatMap.Clear();
+			await Dispatcher.Invoke(async () => {
 
-			Random rRand = new Random();
+				cHeatMap.Clear();
 
-			// Loop variables
-			int x;
-			int y;
-			byte intense;
 
-			// Lets loop few times and create a random point each iteration
-			for (int i = 0; i < 1000; i++)
-			{
+
+				// Lets loop few times and create a random point each iteration
+
 				// Pick random locations and intensity
-				x = rRand.Next(0, (int)(cHeatMap.ActualWidth - 1));
-				y = rRand.Next(0, (int)(cHeatMap.ActualHeight - 1));
-				intense = (byte)rRand.Next(0, 255);
+				//x = rRand.Next(0, (int)(cHeatMap.ActualWidth - 1));
+				//y = rRand.Next(0, (int)(cHeatMap.ActualHeight - 1));
+				//intense = (byte)rRand.Next(0, 255);
+
+				x = 100;
+				y = 100;
+				intense = (byte)255;
 
 				// Add heat point to heat points list
 				cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
-			}
 
-			cHeatMap.Render();
+
+				cHeatMap.Render();
+
+
+				for (int i = 0; i < 10; i++)
+				{
+					x = rRand.Next(0, (int)(cHeatMap.ActualWidth - 1));
+					y = rRand.Next(0, (int)(cHeatMap.ActualHeight - 1));
+					intense = (byte)rRand.Next(0, 255);
+					cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+
+
+					cHeatMap.Render();
+
+				}
+
+			});
+
+			
+
 		}
-	}
+}
+	
 }
