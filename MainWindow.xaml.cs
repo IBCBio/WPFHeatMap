0cf994a867ce87610d93e2f89c6510e6ad9f5c9c
Iván Jiménez <908544+ivanjimenez@users.noreply.github.com>
2023-10-24 12:14:42 +0200

    First commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Timers;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WPFHeatMap
 {
@@ -12,7 +13,8 @@ namespace WPFHeatMap
 
 	public partial class MainWindow : Window
 	{
-		
+		int count = 0;
+
 		Random rRand = new Random();
 
 		// Loop variables
@@ -24,11 +26,57 @@ namespace WPFHeatMap
 			InitializeComponent();
 
 			cbColors.SelectedIndex = 4;
+			DispatcherTimer timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromMilliseconds(10);
+			timer.Tick += timer_Tick;
+			timer.Start();
 
-			
 		}
 
-	
+	private void timer_Tick(object sender, EventArgs e)
+	{
+			
+			x = rRand.Next(100, (int)(111 - 1));
+			y = rRand.Next(100, (int)(111 - 1));
+			intense = (byte)rRand.Next(0, 80);
+			cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+
+
+			x = rRand.Next(110, (int)(121 - 1));
+			y = rRand.Next(105, (int)(121 - 1));
+			intense = (byte)rRand.Next(0, 80);
+			cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+			x = rRand.Next(115, (int)(121 - 1));
+			y = rRand.Next(110, (int)(121 - 1));
+			intense = (byte)rRand.Next(0, 80);
+			cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+
+			x = rRand.Next(130, (int)(135 - 1));
+			y = rRand.Next(165, (int)(185 - 1));
+			intense = (byte)rRand.Next(0, 105);
+			cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+
+
+			x = rRand.Next(135, (int)(145 - 1));
+			y = rRand.Next(165, (int)(185 - 1));
+			intense = (byte)rRand.Next(0, 85);
+			cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+
+			x = rRand.Next(100, (int)(125 - 1));
+			y = rRand.Next(200, (int)(225 - 1));
+			intense = (byte)rRand.Next(0, 105);
+			cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+
+			cHeatMap.Render();
+
+			count++;
+			if (count == 100)
+			{
+				cHeatMap.Clear();
+				count = 0;
+			}
+			
+		}
 
 	private async void Window_Loaded(object sender, RoutedEventArgs e)
 		{
@@ -58,29 +106,23 @@ namespace WPFHeatMap
 				//x = rRand.Next(0, (int)(cHeatMap.ActualWidth - 1));
 				//y = rRand.Next(0, (int)(cHeatMap.ActualHeight - 1));
 				//intense = (byte)rRand.Next(0, 255);
-
-				x = 100;
-				y = 100;
-				intense = (byte)255;
-
-				// Add heat point to heat points list
-				cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+				//cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
 
 
-				cHeatMap.Render();
+				//cHeatMap.Render();
 
 
-				for (int i = 0; i < 10; i++)
-				{
-					x = rRand.Next(0, (int)(cHeatMap.ActualWidth - 1));
-					y = rRand.Next(0, (int)(cHeatMap.ActualHeight - 1));
-					intense = (byte)rRand.Next(0, 255);
-					cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
+				//for (int i = 0; i < 10; i++)
+				//{
+				//	x = rRand.Next(0, (int)(cHeatMap.ActualWidth - 1));
+				//	y = rRand.Next(0, (int)(cHeatMap.ActualHeight - 1));
+				//	intense = (byte)rRand.Next(0, 255);
+				//	cHeatMap.AddHeatPoint(new HeatPoint(x, y, intense));
 
 
-					cHeatMap.Render();
+				//	cHeatMap.Render();
 
-				}
+				//}
 
 			});
 
